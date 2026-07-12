5929cdb5d2d3b7a1979f6883eab04b89b631ec98
spr1er <gg_von1@outlook.com>
2026-07-12 15:25:31 +0300

    feat: update popup js

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,43 @@
 const grabBtn = document.getElementById('grabBtn');
 
 grabBtn.addEventListener('click', () => {
-  chrome.tabs.query({ active: true }, tabs => {
-    const tab = tabs[0];
-    if (tab) {
-      alert(tab.id);
-    } else {
-      alert('There are no active tabs');
-    }
-  });
+    chrome.tabs.query({ active: true }, tabs => {
+        const tab = tabs[0];
+
+        if (tab) {
+            execScript(tab);
+        } else {
+            alert('There are no active tabs');
+        }
+    });
 });
+
+const grabImages = () => {
+    const images = document.querySelectorAll('img');
+    return Array.from(images).map(image => image.src);
+};
+
+const onResult = frames => {
+    if (!frames || !frames.length) {
+        alert('Could not retrieve images from specified page');
+        return;
+    }
+
+    const imageUrls = frames
+        .map(frame => frame.result)
+        .reduce((r1, r2) => r1.concat(r2));
+
+    window.navigator.clipboard
+        .writeText(imageUrls.join('\n'))
+        .then(() => window.close);
+};
+
+const execScript = (tab) => {
+    chrome.scripting.executeScript(
+        {
+            target: { tabId: tab.id, allFrames: true },
+            func: grabImages,
+        },
+        onResult
+    );
+};
